bb345082054e05d2962d2366791e9d28db834700
EugenioCaiazz0 <eugeniodanilenko@icloud.com>
2024-03-19 14:57:29 +0100

    Api call

---
+++ assets/scripts/script.js
@@ -0,0 +1,27 @@
+
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return{
+            apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
+            output: ''
+
+        }
+    },
+
+    methods:{
+        getApis(){
+            axios.get(this.apiUrl)
+            .then(res => {
+                this.output = res.data.response
+            })
+            .catch(this.output = 'Error!')
+        }
+    },
+
+    mounted(){
+        this.getApis()
+    }
+
+}).mount('#app');
