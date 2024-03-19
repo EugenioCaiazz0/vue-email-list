794ba8cc94a3362f95e24d25f813f79c687fbc4f
EugenioCaiazz0 <eugeniodanilenko@icloud.com>
2024-03-19 15:26:14 +0100

    Add cycle

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,18 +5,20 @@ createApp({
     data(){
         return{
             apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
-            output: ''
-
+            output: '',
+            indice: 10,
         }
     },
 
     methods:{
         getApis(){
+            for(let i = 1; i<=10; i++){
             axios.get(this.apiUrl)
             .then(res => {
                 this.output = res.data.response
             })
             .catch(this.output = 'Error!')
+            }
         }
     },
 
@@ -25,3 +27,5 @@ createApp({
     }
 
 }).mount('#app');
+
+
