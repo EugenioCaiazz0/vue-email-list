5ba6af8e31e765d22b2335968db802db8d4d94cb
EugenioCaiazz0 <eugeniodanilenko@icloud.com>
2024-03-19 15:44:00 +0100

    Cycled

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,7 +6,7 @@ createApp({
         return{
             apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
             output: '',
-            indice: 10,
+           eMails: [],
         }
     },
 
@@ -15,7 +15,7 @@ createApp({
             for(let i = 1; i<=10; i++){
             axios.get(this.apiUrl)
             .then(res => {
-                this.output = res.data.response
+                this.eMails.push(res.data.response)
             })
             .catch(this.output = 'Error!')
             }
